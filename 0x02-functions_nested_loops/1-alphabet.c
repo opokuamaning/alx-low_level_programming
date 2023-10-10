2450aa743e8012e366ae0571af9d168f9992fbde
opokuamaning <princeopokuamaning@gmail.com>
2023-10-10 16:23:12 -0700

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print lower case alphabets
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
