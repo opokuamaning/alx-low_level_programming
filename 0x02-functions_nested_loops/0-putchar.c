7120d6d7472d2d3af3c42f5ee6193e3ad02b5a57
opokuamaning <princeopokuamaning@gmail.com>
2023-10-10 15:47:44 -0700

    Added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * prints _putchar, followed by a new line.
+ * The program should return 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
