b3ed03a3678d21e9ed2aabd17a0840a706bd778f
opokuamaning <princeopokuamaning@gmail.com>
2023-10-10 20:33:20 -0700

    Added 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,46 @@
 #include "main.h"
+
 /**
  * print_times_table - print multiplication table up to n
  * @n: integer argument
  */
+
 void print_times_table(int n)
 {
-	int row, col, mul;
+	int row;
+	int column;
+	int product;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (row = 0; row <= n; row++)
 		{
-			for (col = 0; col <= n; col++)
+			for (column = 0; column <= n; column++)
 			{
-				mul = (row * col);
-
-				if (col == 0)
-				{
-					_putchar('0' + mul);
-				}
+				product = (row * column);
+				if (column == 0)
+					_putchar('0' + product);
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
-					if (mul <= 9)
+					if (product <= 9)
 					{
 						_putchar(' ');
 						_putchar(' ');
-						_putchar('0' + mul);
+						_putchar('0' + product);
 					}
-					else if (mul > 9 && mul < 00)
+					else if (product > 9 && product < 100)
 					{
 						_putchar(' ');
-						_putchar('0' + (mul / 10));
-						_putchar('0' + (mul % 10));
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
 					}
-					else if (mul >= 100)
+					else if (product >= 100)
 					{
-						_putchar('0' + (mul / 100));
-						_putchar('0' + ((mul / 10) % 10));
-						_putchar('0' + (mul % 10));
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
 					}
 				}
 			}
