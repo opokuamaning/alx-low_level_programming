3aab9cb19ac738b1fe730895960aefef570aec72
opokuamaning <princeopokuamaning@gmail.com>
2023-10-12 17:54:29 -0700

    Added 5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbersb - prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+			{
+				_putchar((i /10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
