a67bfd86f22712bb0b5b0f085d180a6a27d1a4f3
opokuamaning <princeopokuamaning@gmail.com>
2023-10-12 17:13:25 -0700

    Added 3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * Return: list of numbers
+ */
+
+void print_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
