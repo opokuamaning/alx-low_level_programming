651f3e695165c233002eade31ef1529c5179e147
opokuamaning <princeopokuamaning@gmail.com>
2023-10-12 18:33:56 -0700

    Added 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line on the termina
+ * @n: funtion parameter
+ * Return: \ diagoannly
+ */
+void print_diagonal(int n)
+{
+	 int i, j;
+
+	 if (n > 0)
+	 {
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	 }
+	 else
+	 {
+		_putchar('\n');
+	 }
+}
