ae1a95ea721ced4cddf75fcd70be7e7c1bddee4a
opokuamaning <princeopokuamaning@gmail.com>
2023-10-19 14:45:21 -0700

    Added 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: the integer to print
+ */
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 10)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar('0' + n % 10);
+}
