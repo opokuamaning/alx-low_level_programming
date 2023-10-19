6161027de5e698777ffe9632c2f1b6758e11fb1f
opokuamaning <princeopokuamaning@gmail.com>
2023-10-19 13:49:18 -0700

    Added 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * ro13 - encodes a string using ROT13
+ * @str: input string to encode
+ * Return: string to modify
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		char c = str[i];
+
+		if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		{
+			/* Apply ROT13 */
+			if ((c >= 'A' && c <= 'Z'))
+			{
+				c = (c - 'A' + 13) % 26 + 'A';
+			}
+			else
+			{
+				c = (c - 'a' + 13) % 26 + 'a';
+			}
+			str[i] = c;
+		}
+	}
+	return (str);
+}
