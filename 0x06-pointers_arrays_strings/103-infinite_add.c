c8999d08d5ab46225861915d353bb70b5d5c10a3
opokuamaning <princeopokuamaning@gmail.com>
2023-10-19 17:30:10 -0700

    Added  103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,76 +1,54 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * infinite_add - adds two number represented as strings
- * @n1: first number as a string
- * @n2: second number as a string
- * @r: buffer to store the result
- * @size_r: size of the result buffer
- * Return: pointer to result
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
- char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0;
-	int len2 = 0;
-	int carry = 0;
-	int i, j, k;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (n1[len1] != '\0')
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
+	else
+		bg = c2;
+	if (size_r <= bg + 1)
+		return (0);
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		len1++;
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
+		else
+			add = 0;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
+		else
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
 	}
-	 while (n2[len2] != '\0')
-        {
-                len2++;
-        }
-
-	 if (len1 + 2 > size_r || len2 + 2 > size_r)
-	 {
-	 	return (0);
-	 }
-
-	 i = len1 - 1;
-	 j = len2 - 1;
-	 k = 0;
-
-	 while (i >= 0|| j >= 0 || carry)
-	 {
-	 	int num1 = (i >= 0) ? n1[i] - '0' : 0;
-		int num2 = (j >= 0) ? n2[j] - '0' : 0;
-		int sum = num1 + num2 + carry;
-
-		if (k < size_r - 1)
-		{
-			r[k] = (sum % 10) + '0';
-			k++;
-		}
-		else
-		{
-			return 0;
-		}
-
-		carry = sum / 10;
-		i--;
-		j--;
-	 }
-
-	 if (carry > 0)
-	 {
-	 	if (k < size_r - 1)
-		{
-			r[k] = carry + '0';
-			k++;
-		}
-		else
-		{
-			return (0);
-		}
-	 }
-	 r[k] = 0;
-	 for (i = 0, j = k - 1; i < j; i++, j--)
-	 {
-	 	char temp = r[i];
-		r[i] = r[j];
-		r[j] = temp;
-	 }
-	 return (r);
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
