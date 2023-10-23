476b25f4e7807a7f4f092a3037e970718576e2f6
opokuamaning <princeopokuamaning@gmail.com>
2023-10-23 14:53:46 -0700

    Added 0-memset.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - spoies memory area
+ * @dest: destination memery area
+ * @src: source memory area
+ * @n: number of bytes to copy
+ * Return: pointer
+ */
+char *memcpr(char * dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
