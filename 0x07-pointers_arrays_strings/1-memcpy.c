d9aeacff5811ce22b798ce88fbd9f86452091965
opokuamaning <princeopokuamaning@gmail.com>
2023-10-23 17:38:10 -0700

    Added 1-main.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  * @n: number of bytes to copy
  * Return: pointer
  */
-char *memcpr(char * dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
