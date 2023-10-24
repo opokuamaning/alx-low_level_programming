7aa747acc10e322002e3f04bd8c8138bbd075ed5
opokuamaning <princeopokuamaning@gmail.com>
2023-10-23 17:13:58 -0700

    Added 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,14 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		s[i] = b;
+		unsigned int i;
+
+		for (i = 0; i < n; i++)
+		{
+			s[i] = b;
+		}
 	}
 
 	return (s);
