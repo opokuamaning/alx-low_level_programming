a60b211d78d46589cc5afe39e062c89418800347
opokuamaning <princeopokuamaning@gmail.com>
2023-11-02 16:57:25 -0700

    Added 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: Number of elements in the array
+ * @size: size of each element in bytes
+ * Return: Pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int total_size;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	total_size = nmemb * size;
+
+	ptr = malloc(total_size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < total_size; i++)
+	{
+		((char*)ptr)[i] = 0;
+	}
+
+	return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,7 @@ int _putchar(char c);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
+void *_calloc(unsigned int nmemb, unsigned int size);
+
 
 #endif
