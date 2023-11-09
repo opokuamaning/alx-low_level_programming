0b6e25e87cb80c27d8f4666d19f79124955ae9ea
opokuamaning <princeopokuamaning@gmail.com>
2023-11-08 16:23:38 -0800

    Added new updates

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,16 +1,19 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef PROTOTYPES
+#define PROTOTYPES
+
 /**
  * struct op - Struct op
  *
  * @op: The operator
  * @f: The function associated
  */
+
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
+
 /* functions to choose from for 5 different operations */
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -20,4 +23,5 @@ int op_mod(int a, int b);
 
 /* function to select correct operation function to perform */
 int (*get_op_func(char *s))(int, int);
+
 #endif
