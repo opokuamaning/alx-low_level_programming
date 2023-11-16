997ac9e1764c84a1389d9c0af92bfe85e4fe2e92
opokuamaning <princeopokuamaning@gmail.com>
2023-11-16 14:37:06 -0800

    Debugged 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 /**
  * premain - Prints a specific message before ain is executed
  */
-void __attribute__((constrcutor)) premain(void)
+void __attribute__((constructor)) premain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
