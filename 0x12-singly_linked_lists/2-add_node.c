7b9f48dc91b101cb57c7d3884b53522f477b59b8
opokuamaning <princeopokuamaning@gmail.com>
2023-11-16 13:54:31 -0800

    Checked for betty compliace

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -23,12 +23,12 @@ int _strlen(const char *str)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	
+
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	new_node->str = strdup(str);
 
