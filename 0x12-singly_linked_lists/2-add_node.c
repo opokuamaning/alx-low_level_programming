8f6794bd4b8cad2b29e6857e316e147975485f50
opokuamaning <princeopokuamaning@gmail.com>
2023-11-16 13:16:52 -0800

    Added 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node: Adds a new node at the beginning of a list
+ * @head: Double pointer to the head of the linked list
+ * @str: String to be duplicated and added to the new node
+ * Return: The address of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return NULL;
+	}
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,9 @@
 #define LIST_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,6 +22,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-
+list_t *add_node(list_t **head, const char *str);
 
 #endif
