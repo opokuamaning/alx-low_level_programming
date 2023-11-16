fc1e9b4b1754878b6cf851452bb951ef4b81d298
opokuamaning <princeopokuamaning@gmail.com>
2023-11-16 14:28:19 -0800

    Adde w00-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * premain - Prints a specific message before ain is executed
+ */
+void __attribute__((constrcutor)) premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
