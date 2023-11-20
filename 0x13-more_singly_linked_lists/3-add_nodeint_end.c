80bb222f0b7019b6b08999ac632cc4e06bb6fb70
opokuamaning <princeopokuamaning@gmail.com>
2023-11-20 14:26:33 -0800

    checked for betty compliance

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,15 +10,15 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new_node, *temp;
 
 	new_node = malloc(sizeof(listint_t));
-	if(new_node == NULL)
+	if (new_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if(*head == NULL)
+	if (*head == NULL)
 	{
 		*head = new_node;
 	}
