13720f32d11a8d5a8f88a715b0f96da612b85579
opokuamaning <princeopokuamaning@gmail.com>
2023-11-21 14:38:27 -0800

    Modified 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		nnodes++;
 	}
-
-	return (count);
+	return (nnodes);
 }
