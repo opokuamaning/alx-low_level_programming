aceabab6b78ee3af9ced624cb588beee4a439143
opokuamaning <princeopokuamaning@gmail.com>
2023-11-20 17:53:33 -0800

    Checked for betty compliance

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,14 +15,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-
 	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
 	new_node->n = n;
 
 	if (idx == 0)
@@ -31,22 +29,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-
 	current = *head;
 
 	for (i = 0; i < idx - 1 && current != NULL; i++)
 	{
 		current = current->next;
 	}
-
 	if (current == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->next = current->next;
 	current->next = new_node;
-
 	return (new_node);
 }
