41c23551ffe7769e9b4add03d735351a23db6ee9
opokuamaning <princeopokuamaning@gmail.com>
2023-11-20 17:36:54 -0800

    Checked for betty compliance

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_listint - Returns the sum of all the data (n) 
+ * sum_listint - Returns the sum of all the data (n)
  * of a listint_t linked list.
  * @head: Pointer to the head of the list.
  * Return: The sum of all the data (n) or 0 if the list is empty.
