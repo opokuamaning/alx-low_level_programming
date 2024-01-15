e36ca4f9ecc69ff7aed4e03fbf6359aba622924d
opokuamaning <princeopokuamaning@gmail.com>
2024-01-15 12:44:23 -0800

    Added 100-operation.so

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a: first int
+ * @b: second int
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtraction
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * div - divide two integers
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - subtract two integers
+ * @a: first int
+ * @b: second int
+ * Return: Modula
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
